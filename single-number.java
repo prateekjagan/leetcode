98b609b298005bf89b296bdb915df759aa7cac24
Prateek Jagan <71245667+prateekjagan@users.noreply.github.com>
2020-12-31 00:57:52 +0530

    Create single-number.java

---
+++ single-number.java
@@ -0,0 +1,16 @@
+/*
+https://leetcode.com/problems/single-number/
+31/12/20
+*/
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+
+}
