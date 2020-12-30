28610f5119154636f4b02c022605b99580481c78
Prateek Jagan <71245667+prateekjagan@users.noreply.github.com>
2020-12-31 00:16:04 +0530

    Create contains-duplicate.java

---
+++ contains-duplicate.java
@@ -0,0 +1,12 @@
+/*
+https://leetcode.com/problems/contains-duplicate/
+https://leetcode.com/problems/contains-duplicate/solution/
+
+
+Approach #1 (Naive Linear Search) [Time Limit Exceeded]
+Approach #2 (Sorting) [Accepted]
+Approach #3 (Hash Table) [Accepted]
+
+#TODO
+Write code for Hash table
+*/
