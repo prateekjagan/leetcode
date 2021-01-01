937fbd3b050b4d96a1d0250c4575811104deba51
Prateek Jagan <71245667+prateekjagan@users.noreply.github.com>
2021-01-01 22:15:14 +0530

    Create minimum-number-of-steps-to-make-two-strings-anagram.java

---
+++ minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -0,0 +1,27 @@
+/*
+https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
+01/01/21
+*/
+
+class Solution {
+    public int minSteps(String s, String t) {
+        int minChar = s.length();
+        HashMap<Character, Integer> anagram = new HashMap<>();
+        for(char i : s.toCharArray()){
+            if (anagram.containsKey(i))
+                anagram.put(i, anagram.get(i)+1);
+            else
+                anagram.put(i,1);
+        }
+        for(char i : t.toCharArray())
+        {
+            if(anagram.containsKey(i) && anagram.get(i)!=0)
+            {
+                anagram.put(i, anagram.get(i)-1);
+                minChar--;
+            }
+                
+        }
+      return minChar;
+    }
+}
